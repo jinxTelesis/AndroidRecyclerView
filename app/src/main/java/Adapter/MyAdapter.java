1ef5e3ceea014e0036aaa6c9632e726dbc7688e8
jinxTelesis <Dremon7@hotmail.com>
2018-10-08 19:59:32 -0400

    Initial commit

---
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -0,0 +1,79 @@
+package Adapter;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dre.androidrecyclerview.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import Model.ListItem;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+    private Context context;
+    private List<ListItem> listItems;
+
+    public MyAdapter(Context context, List listItem)
+    {
+        this.context = context;
+        this.listItems = listItem; // need to inflate xml file and make
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+
+        ListItem item = listItems.get(position);
+        holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
+        holder.rating.setText(item.getRating());
+
+
+    }
+
+    @Override
+    public int getItemCount() { // need to return size of list items
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView name;
+        public TextView description;
+        public TextView rating;
+
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
+            rating = (TextView) itemView.findViewById(R.id.rating);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+
+            ListItem item = listItems.get(position);
+
+            Toast.makeText(context, item.getName(), Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/Model/ListItem.java
@@ -0,0 +1,48 @@
+package Model;
+
+/**
+ * Create by Andre Lussier on 10/7/2018
+ */
+
+public class ListItem {
+    private String name;
+    private String description;
+    private String rating;
+
+    public ListItem(String name, String description, String rating){
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
+
+
+}
